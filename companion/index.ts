59eb51c443022550ccfd9b365f34bade2a1d40a5
Billy M <bmihalarias@gmail.com>
2022-02-07 15:47:13 +1100

    added .env for storage url configuration

--- companion/index.ts
+++ companion/index.ts
@@ -1,5 +1,10 @@
 import { ErrorEvent, MessageEvent, peerSocket } from "messaging";
 
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const storageURL = process.env["STORAGE_URL"]
+
 peerSocket.onerror = (err : ErrorEvent) => {
   // Handle any errors
   console.log("Connection error: " + err.code + " - " + err.message);
