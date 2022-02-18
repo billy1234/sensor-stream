d4c50a197051ebc2ef42f8687d60b586f3227784
Billy M <bmihalarias@gmail.com>
2022-02-18 14:31:21 +1100

    API now connects to mongodb

--- database-service/sensor-store-api/src/handler.ts
+++ database-service/sensor-store-api/src/handler.ts
@@ -1,13 +1,20 @@
-import { Handler, Context } from 'aws-lambda';
+import { APIGatewayProxyHandler, APIGatewayProxyEvent } from 'aws-lambda';
 import {StoreCollection} from './services/sensor-db';
 
 import * as dotenv from "dotenv";
 dotenv.config()
 
 
-export const store : Handler = async (event : any, context: Context) => {
+export const store : APIGatewayProxyHandler = async (event : APIGatewayProxyEvent) => {
+  var data = JSON.parse(event.body)["data"]
+
+  data = data.map(x => {
+    x._id = x["dateTime"] + x["user"]
+    return x
+  });
+
   try {
-    await StoreCollection(event["data"])
+    await StoreCollection(data)
     return {
       statusCode: 201,
       body: JSON.stringify(
--- database-service/sensor-store-api/src/services/sensor-db.ts
+++ database-service/sensor-store-api/src/services/sensor-db.ts
@@ -1,14 +1,19 @@
-import { MongoClient} from 'mongodb'
-
-const connection_string = `mongodb+srv://${process.env["DB_USER"]}:${process.env["DB_PASSWORD"]}@${process.env["DB_URL"]}/myFirstDatabase?retryWrites=true&w=majority`
-const client = new MongoClient(connection_string)
-const db_name = process.env["DB_NAME"]
-const collection_name = process.env["COLLECTION_NAME"]
-
+import {MongoClient} from 'mongodb'
 
 export async function StoreCollection  (data: any[]) : Promise<void>{
-    const conn = await client.connect()
-    conn.db(db_name).collection(collection_name).insertMany(data)
-    conn.close()
+    const conn = await getClient().connect()
 
+    await conn.db(process.env["DB_NAME"])
+        .collection(process.env["COLLECTION_NAME"])
+        .insertMany(data)
+
+    await conn.close()
+
+}
+
+function getClient() : MongoClient{
+    if (!(process.env["DB_USER"] && process.env["DB_PASSWORD"] && process.env["DB_URL"] && process.env["DB_NAME"] && process.env["COLLECTION_NAME"]))
+        throw `Enviroment variable error. Check .env file or enviroment variables DB_USER:${process.env["DB_USER"]} DB_PASSWORD:${process.env["DB_PASSWORD"]} DB_URL:${process.env["DB_URL"]} DB_NAME:${process.env["DB_NAME"]} COLLECTION_NAME:${process.env["COLLECTION_NAME"]}` 
+
+    return new MongoClient(`mongodb+srv://${process.env["DB_USER"]}:${process.env["DB_PASSWORD"]}@${process.env["DB_URL"]}/myFirstDatabase?retryWrites=true&w=majority`)
 }
